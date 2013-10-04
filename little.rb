80777d1f75937639c05379dd004725bb0bd46864
Simon Taranto <simon.taranto@gmail.com>
2013-10-04 15:24:55 -0600

    Add initial most_items call

--- little.rb
+++ little.rb
@@ -4,7 +4,7 @@ require 'bigdecimal'
 Shoes.app do
   @mr = SalesEngine.new("../sales_engine/data").merchant_repository
   stack(margin: 8) do
-    para "Enter a merchant id"
+    para "Enter a merchant id to see that merchant's name"
     flow do
       @input = edit_line
       @merchant = button "OK"
@@ -13,5 +13,16 @@ Shoes.app do
       @merchant.click do
         @text.replace @mr.find_all_by_id(@input.text).first.name
      end
+    para "Press OK to see the merchant name with the most items sold"
+    flow do
+      @input_items = edit_line
+      @merchant_items = button "OK"
+    end
+      @text_items = para ""
+      @merchant_items.click do
+        @text_items.replace @mr.most_items(@input_items.text.to_i).first.name
+     end
+
+
   end
 end
