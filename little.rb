60783c2eb9bd4b66728095b60f1a342b44607dec
Simon Taranto <simon.taranto@gmail.com>
2013-10-04 15:05:58 -0600

    first ui

---
+++ little.rb
@@ -0,0 +1,17 @@
+require '../sales_engine/lib/sales_engine'
+require 'bigdecimal'
+
+Shoes.app do
+  @mr = SalesEngine.new("../sales_engine/data").merchant_repository
+  stack(margin: 8) do
+    para "Enter a merchant id"
+    flow do
+      @input = edit_line
+      @merchant = button "OK"
+    end
+      @text = para ""
+      @merchant.click do
+        @text.replace @mr.find_all_by_id(@input.text).first.name
+    end
+  end
+end
